429ffb6262e3a725a648b9b550fe857a6c1c8a1f
alina <todirel.alina@gmail.com>
2021-03-29 14:40:41 -0400

    modified text

--- src/main/java/springbootstarter/controllers/HelloController.java
+++ src/main/java/springbootstarter/controllers/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @RequestMapping("/hello")
     //@RequestMapping defined in the class level maps a specific request path or pattern to a controller.
     public String sayHi(){
-        return "Hi";
+        return "Hello there";
     }
 }
