315492ebb3bc846b77d17343f634b7a771b0446f
alina <todirel.alina@gmail.com>
2021-03-29 12:45:17 -0400

    first commit

---
+++ src/main/java/springbootstarter/controllers/TopicRepository.java
@@ -0,0 +1,14 @@
+package springbootstarter.controllers;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+
+
+    //getAllTopics()
+    //getTopic(String id)
+    //updateTopic(Topic t)
+    //deleteTopic(String id)
+}
